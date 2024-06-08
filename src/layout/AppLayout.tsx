baa19845a67d85aaaf1f6264d9a4f902de75aa00
ammar afzal <ammarafzal.webdeveloper@gmail.com>
2024-06-08 16:20:28 +0500

    applayout updated

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,7 +1,25 @@
-const AppLayout = () => {
-  return (
-    <div>AppLayout</div>
-  )
-}
+import React from "react";
+import { CssBaseline, Box, Container } from "@mui/material";
+import Header from "./Header";
+import Drawer from "./Sidebar";
 
-export default AppLayout
+type AppLayoutProps = {
+  children: React.ReactNode;
+};
+const AppLayout = ({ children }: AppLayoutProps) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Header />
+      <Drawer />
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Container maxWidth="lg">{children}</Container>
+      </Box>
+    </Box>
+  );
+};
+
+export default AppLayout;
