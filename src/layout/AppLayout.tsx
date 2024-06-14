f57b5cb80430c716aebcbbc65c29cb30644c877a
ammar afzal <ammarafzal.webdeveloper@gmail.com>
2024-06-14 17:51:35 +0500

    layout updated

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -268,7 +268,6 @@ export default function MiniDrawer() {
           Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
           Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
           feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec
           massa sapien faucibus et molestie ac. Check the content of app.
         </Typography>
         <Typography sx={{ marginBottom: 2 }}>
