8440afd27db1e133d5f77d42e08a273f31e38ad5
ammar afzal <ammarafzal.webdeveloper@gmail.com>
2024-12-10 17:45:03 +0500

    signup page added

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,8 +1,119 @@
-import { Box } from '@mui/material'
-const Signup = () => {
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Box,
+  Typography,
+  Grid2
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+function Copyright() {
   return (
-    <Box>Signup</Box>
-  )
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Signup
+export default function SignUp() {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",  
+      }}
+    >
+      <CssBaseline />
+      <Box sx={{ mt: 8, display: "flex", flexDirection: "column", alignItems: "center" , justifyContent:"center"}}>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate sx={{ width: "100%", mt: 3 }}>
+          <Grid2 container spacing={2}>
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid2>
+            <Grid2  size={{ xs: 12, sm: 6 }}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid2>
+            <Grid2 size={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid2>
+            <Grid2 size={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid2>
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions, and updates via email."
+              />
+            </Grid2>
+          </Grid2>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign Up
+          </Button>
+          <Grid2 container justifyContent="flex-end">
+            <Box>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Box>
+          </Grid2>
+        </Box>
+      </Box>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Box>
+  );
+}
