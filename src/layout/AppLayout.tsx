f46460cdc4e711109837a56cf6b2e7bafebac5be
ammar afzal <ammarafzal.webdeveloper@gmail.com>
2025-01-20 09:11:47 +0500

    drawer code commented in app layout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,61 +1,64 @@
 import * as React from "react";
-import { styled, Theme, CSSObject } from "@mui/material/styles";
+import {
+  styled,
+  // Theme, CSSObject
+} from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
+// import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
+// import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
+// import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InventoryIcon from "@mui/icons-material/Inventory";
-import RateReviewIcon from "@mui/icons-material/RateReview";
-import CategoryIcon from "@mui/icons-material/Category";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import { useNavigate } from "react-router";
-import { Button } from "@mui/material";
+// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import InventoryIcon from "@mui/icons-material/Inventory";
+// import RateReviewIcon from "@mui/icons-material/RateReview";
+// import CategoryIcon from "@mui/icons-material/Category";
+// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import DashboardIcon from "@mui/icons-material/Dashboard";
+// import { useNavigate } from "react-router";
+// import { Button } from "@mui/material";
 import SideBar from "./Sidebar";
 
 const drawerWidth = 240;
 
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
+// const openedMixin = (theme: Theme): CSSObject => ({
+//   width: drawerWidth,
+//   transition: theme.transitions.create("width", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.enteringScreen,
+//   }),
+//   overflowX: "hidden",
+// });
 
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
+// const closedMixin = (theme: Theme): CSSObject => ({
+//   transition: theme.transitions.create("width", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   overflowX: "hidden",
+//   width: `calc(${theme.spacing(7)} + 1px)`,
+//   [theme.breakpoints.up("sm")]: {
+//     width: `calc(${theme.spacing(8)} + 1px)`,
+//   },
+// });
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
+// const DrawerHeader = styled("div")(({ theme }) => ({
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "flex-end",
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+// }));
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
@@ -84,30 +87,30 @@ const AppBar = styled(MuiAppBar, {
   ],
 }));
 
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-      },
-    },
-    {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-      },
-    },
-  ],
-}));
+// const Drawer = styled(MuiDrawer, {
+//   shouldForwardProp: (prop) => prop !== "open",
+// })(({ theme }) => ({
+//   width: drawerWidth,
+//   flexShrink: 0,
+//   whiteSpace: "nowrap",
+//   boxSizing: "border-box",
+//   variants: [
+//     {
+//       props: ({ open }) => open,
+//       style: {
+//         ...openedMixin(theme),
+//         "& .MuiDrawer-paper": openedMixin(theme),
+//       },
+//     },
+//     {
+//       props: ({ open }) => !open,
+//       style: {
+//         ...closedMixin(theme),
+//         "& .MuiDrawer-paper": closedMixin(theme),
+//       },
+//     },
+//   ],
+// }));
 interface AppLayoutProps {
   children: React.ReactNode;
 }
@@ -115,15 +118,15 @@ interface AppLayoutProps {
 export default function AppLayout({ children }: AppLayoutProps) {
   // const theme = useTheme();
   const [open, setOpen] = React.useState(true);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleDrawerOpen = () => {
     setOpen(true);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(!open);
-  };
+  // const handleDrawerClose = () => {
+  //   setOpen(!open);
+  // };
   // const menuItems = [
   //   { name: "Dashboard", path: "/dashboard", icon: <DashboardIcon /> },
   //   { name: "Products", path: "/products", icon: <InventoryIcon /> },
