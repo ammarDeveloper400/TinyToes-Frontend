79fc1038e4fc7c345a17e11768d2bf72c6b01b0e
ammar afzal <ammarafzal.webdeveloper@gmail.com>
2025-02-10 09:18:28 +0500

    Card added on dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,50 @@
+import { Box, Card, Grid, Typography } from "@mui/material";
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card sx={{ height: "100%", padding: "2rem" }}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: "bold" }}>
+                Total Products
+              </Typography>
+              <Typography sx={{ fontWeight: "bold" }}>0</Typography>
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card>
+            <Typography>Total Products</Typography>
+          </Card>
+        </Grid>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card>
+            <Typography>Total Products</Typography>
+          </Card>
+        </Grid>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card>
+            <Typography>Total Products</Typography>
+          </Card>
+        </Grid>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card>
+            <Typography>Total Products</Typography>
+          </Card>
+        </Grid>
+        <Grid item sm={12} md={6} lg={2} xl={2}>
+          <Card>
+            <Typography>Total Products</Typography>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
